fdbfa69932508fd9c52406449f401a510f6fa205
helenaqin3956 <helenaqin3956@gmail.com>
2017-10-10 13:32:09 -0400

    new updates

--- create_profile.php
+++ create_profile.php
@@ -15,8 +15,8 @@
 
 	<div>
 		<p>
-			<lable for="profilepic"> Upload a profile picture: </lable>
-			<input type="file" name="fileToUpload" id="fileToUpload">
+			<lable for="profilepic"> Choose a profile picture: </lable>
+			<input type="" name="fileToUpload" id="fileToUpload">
 <!--			<input type="submit" value="Upload Image" name="submit3">-->
 		</p>
 	</div>
@@ -100,7 +100,8 @@ if ( isset( $_POST[ "submit" ] ) ) {
 
 			echo "Your account has been generated successfully.<br>";
 
-			$_SESSION[ 'login_user' ] = $myusername;
+			//$_SESSION[ 'login_user' ] = $myusername;
+			setcookie("username", $myusername);
 			header( "location: homepage.php" );
 
 		} else {
@@ -110,69 +111,6 @@ if ( isset( $_POST[ "submit" ] ) ) {
 	}
 
 	$conn->close();
-
-//	$file = rand( 1000, 100000 ) . "-" . $_FILES[ 'file' ][ 'name' ];
-//	$file_loc = $_FILES[ 'file' ][ 'tmp_name' ];
-//	$file_size = $_FILES[ 'file' ][ 'size' ];
-//	$file_type = $_FILES[ 'file' ][ 'type' ];
-//	$folder = "uploads/";
-//
-//	move_uploaded_file( $file_loc, $folder . $file );
-//	$sql = "INSERT INTO tbl_uploads(file,type,size) VALUES('$file','$file_type','$file_size')";
-//	mysql_query( $sql );
-	
-	
-	
-	//************* upload pics
-$target_dir = "uploads/";
-	$target_file = $target_dir . basename( $_FILES[ "fileToUpload" ][ "name" ] );
-	$uploadOk = 1;
-	$imageFileType = pathinfo( $target_file, PATHINFO_EXTENSION );	
-
-
-// Check if image file is a actual image 
-	
-	$check = getimagesize( $_FILES[ "fileToUpload" ][ "tmp_name" ] );
-	if ( $check !== false ) {
-		echo "File is an image - " . $check[ "mime" ] . ".";
-		$uploadOk = 1;
-	} else {
-		echo "File is not an image.";
-		$uploadOk = 0;
-	}
-
-// Check if file already exists
-if ( file_exists( $target_file ) ) {
-	echo "Sorry, file already exists.";
-	$uploadOk = 0;
-}
-// Check file size
-if ( $_FILES[ "fileToUpload" ][ "size" ] > 500000 ) {
-	echo "Sorry, your file is too large.";
-	$uploadOk = 0;
-}
-// Allow certain file formats
-if ( $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" &&
-	$imageFileType != "gif" ) {
-	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-	$uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ( $uploadOk == 0 ) {
-	echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-} else {
-	if ( move_uploaded_file( $_FILES[ "fileToUpload" ][ "tmp_name" ], $target_file ) ) {
-		echo "The file " . basename( $_FILES[ "fileToUpload" ][ "name" ] ) . " has been uploaded.";
-	} else {
-		echo "Sorry, there was an error uploading your file.";
-	}
-}
-
-
-
-//******************
-	
 	
 
 }
@@ -183,11 +121,6 @@ if ( isset( $_POST[ "cancel" ] ) ) {
 	header( "location: homepage.php" );
 
 }
-
-
-
-
-
 
 
 
